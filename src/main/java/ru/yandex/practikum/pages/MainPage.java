f140ffbecfdc31c60795f6f2c09b85090f27d2da
Kuznetsov Sergey <kuznecovsa96@yandex.ru>
2025-02-08 17:00:09 +0300

    added negative check order status tests

--- src/main/java/ru/yandex/practikum/pages/MainPage.java
+++ src/main/java/ru/yandex/practikum/pages/MainPage.java
@@ -3,8 +3,13 @@ package ru.yandex.practikum.pages;
 import lombok.AllArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static ru.yandex.practikum.EnvConfig.BASE_URL;
+import static ru.yandex.practikum.EnvConfig.EXPLICIT_WAIT;
 
 @AllArgsConstructor
 public class MainPage {
@@ -17,6 +22,12 @@ public class MainPage {
     private final By yandexLogo = By.className("Header_LogoYandex__3TSOI");
     //Кнопка "Принять куки"
     private final By acceptCookieButton = By.id("rcc-confirm-button");
+    //Кнопка "Статус заказа"
+    private final By orderStatusButton = By.xpath(".//button[text()='Статус заказа']");
+    //Поле ввода номера заказа
+    private final By orderNumberField = By.xpath(".//input[@placeholder='Введите номер заказа']");
+    //Кнопка "Go!"
+    private final By goButton = By.xpath(".//button[text()='Go!']");
     //Верхняя кнопка "Заказать"
     private final By headerOrderButton = By.cssSelector("div.Header_Nav__AGCXC>button.Button_Button__ra12g");
     //Нижняя кнопка "Заказать"
@@ -25,6 +36,20 @@ public class MainPage {
     public static MainPage openMainPage(WebDriver webDriver) {
         webDriver.get(BASE_URL);
         return new MainPage(webDriver);
+    }
+
+    public void clickOrderStatusButton() {
+        webDriver.findElement(orderStatusButton).click();
+    }
+
+    public void fillOrderNumberField(String orderNumber) {
+        webDriver.findElement(orderNumberField).sendKeys(orderNumber);
+    }
+
+    public OrderStatusPage clickGoButton() {
+        new WebDriverWait(webDriver, Duration.ofSeconds(EXPLICIT_WAIT)).until(ExpectedConditions.elementToBeClickable(goButton));
+        webDriver.findElement(goButton).click();
+        return new OrderStatusPage(webDriver);
     }
 
     public void clickScooterLogo() {
